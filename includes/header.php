e270f395387e117dd6b132109d68528510cb0fbf
AAndr3 <andrepedrosaantunes@gmail.com>
2019-05-17 16:12:21 +0100

    mente viva projeto

---
+++ includes/header.php
@@ -0,0 +1,113 @@
+<header>
+
+	<!--HEADER-WRAPPER-->
+
+	<div class="header-wrapper">
+
+		<img src="assets/images/logo.png">
+
+		<div class="informacoes_icone_div">
+
+			<div class="icone_div">
+				<i id="icone" class="fas fa-envelope"></i>
+			</div>
+
+			<h5 id="info_email" class="info">Para mais informações envie um email para<br><span class="important">menteviva@gmail.com</span></h5>
+
+		</div>
+
+		<div class="informacoes_icone_div">
+
+			<div class="icone_div">
+				<i id="icone" class="fas fa-phone"></i>
+			</div>
+
+			<h5 id="info_contato" class="info">Para ajuda ligue para<br><span class="important">960442568</span></h5>
+
+		</div>
+
+		<div class="social_links">
+			<i class="fab fa-facebook-square social_link social"></i>
+		    <i class="fab fa-instagram social_link social"></i>
+		    <i class="fab fa-twitter-square social_link social"></i>
+		    <i class="fab fa-google-plus-square social_link social"></i>
+		</div>
+
+		<button class="entrar_registar">Login/Registar</button>
+
+	</div>
+
+	<!-- /HEADER-WRAPPER-->
+
+	<div class="nav-wrapper">
+		
+		<div class="nav-text">
+			
+			<div class="nav-menu" id="menu">
+				<a href="javascript:void(0);" class="icon" onclick="responsivo_menu()"><i class="fa fa-bars"></i></a>
+				<a href="index.php">Home</a>
+				<a href="#pesquisar">Pesquisar</a>
+				<a href="#vender">Vender veículo</a>
+				<a href="#informacoes">Informações</a>
+			</div>
+
+			<div class="nav-resto">
+				<button onclick="myFunction()" class="btn_profile"><i class="fas fa-user"></i><i style="margin-left: 0.6rem;" class="fas fa-angle-down"></i></button>
+					<div id="dropdown-profile" class="dropdown-content">
+					    <a href="#perfil">Perfil</a>
+					    <a href="#pass">Mudar palavra passe</a>
+					    <a href="#email">Mudar email</a>
+   					    <a href="#sari">Sair</a>
+
+					  </div>
+				<input class="input_search" type="text" placeholder="Pesquisar">
+			</div>
+				
+		</div>
+
+	</div>
+
+</header>
+
+
+<script>
+
+
+function myFunction() {
+	document.getElementById("dropdown-profile").classList.toggle("show");
+}
+
+function responsivo_menu() {
+  var x = document.getElementById("menu");
+  if (x.className === "nav-menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-menu";
+  }
+}
+
+	
+$(window).resize(function() {
+  if ($(window).width() <= 1059) {
+  	document.getElementById("info_email").innerHTML = "menteviva@gmail.com";
+  	document.getElementById("info_contato").innerHTML = "960442568";
+  } else {
+  	document.getElementById("info_email").innerHTML = "Para mais informações envie um email para<br><span class='important'>menteviva@gmail.com</span>";
+  	document.getElementById("info_contato").innerHTML = "Para ajuda ligue para<br><span class='important'>960442568</span>";
+  }
+});
+
+
+$(window).load(function() {
+	$(window).resize(function() {
+	  if ($(window).width() <= 1059) {
+	  	document.getElementById("info_email").innerHTML = "menteviva@gmail.com";
+	  	document.getElementById("info_contato").innerHTML = "960442568";
+	  } else {
+	  	document.getElementById("info_email").innerHTML = "Para mais informações envie um email para<br><span class='important'>menteviva@gmail.com</span>";
+	  	document.getElementById("info_contato").innerHTML = "Para ajuda ligue para<br><span class='important'>960442568</span>";
+	  }
+	});
+});
+
+</script>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+session_start();
+include('includes/config.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+	<!--STYLE-->
+	<link rel="stylesheet" type="text/css" href="assets/css/header.css">
+	<!--/STYLE-->
+	<title>Mente Viva </title>
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+	<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+</head>
+<body>
+
+<!--Header-->
+<?php include('includes/header.php'); ?>
+<!--/Header-->
+
+
+<!--BANNER-->
+<div class="banner">
+	<div class="banner_info">
+		<h1>Escolhe o carro<br>certo para ti</h1>
+		<h3>Temos centenas de carros<br>para escolheres</h3>
+		<button class="search_banner">Ler mais</button>
+	</div>
+</div>
+
+
+
+<!--/BANNER-->
+
+</body>
+</html>
